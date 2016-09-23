dd4fdfd5da48615568927f3cb47e39102b3240df
Andrei K <kost@kost.ru>
2016-09-23 16:52:16 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var postcss = require('postcss');
+var _ = require('underscore');
+ 
+module.exports = postcss.plugin('postCssReplaceProperty', function postCssReplaceProperty(options) {
+ 
+    return function (css) {
+ 		var opts;
+
+        var defaults = {
+        	selector : "",
+        	property : "",
+        	value    : "",
+        	replacement : ""
+        };
+
+        options = options || {};
+
+       opts = _.extend(defaults, options);
+         
+        // Processing code will be added here
+        css.walkRules(opts.selector, function (rule) {
+         
+            rule.walkDecls(opts.property, function (decl, i) {
+         		var value = decl.value;
+         		// height: 1em;
+         		if (value.indexOf( opts.value ) !== -1) {
+         			if(opts.replacement) {
+         				decl.value = opts.replacement;
+         			} else {
+         				decl.remove();
+         			}
+         		}
+         
+            });
+         
+        });
+ 
+    }
+ 
+});
